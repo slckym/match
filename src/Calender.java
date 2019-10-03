d0fee12ef8af12b59c485044e364ceb1f4a834a8
Selçuk Yıldırım <syildirim@isys.com.tr>
2019-10-03 15:56:43 +0300

    Tablo kolonları düzenlendi.

--- src/Calender.java
+++ src/Calender.java
@@ -67,11 +67,12 @@ public class Calender extends JFrame {
         Database db = new Database();
         try {
             String query = "SELECT ma.id AS 'ID'," +
-                    "team1.team_name AS 'Team First', team2.team_name AS 'Team Second', " +
-                    "ma.fp_team1_score AS 'FP Team First Score', ma.fp_team2_score AS 'FP Team Second Score', " +
-                    "ma.sp_team1_score AS 'SP Team First Score', ma.sp_team2_score AS 'SP Team Second Score', " +
-                    "ma.ms_team1 AS 'MS Team First Score', ma.ms_team2 AS 'MS Team Second Score', " +
-                    "ma.match_date as 'Match Date'" +
+                    "team1.team_name AS 'Team First'," +
+                    "team2.team_name AS 'Team Second'," +
+                    "ma.fp_team1_score || '-' || ma.fp_team2_score AS 'FP Score'," +
+                    "ma.sp_team1_score || '-' || ma.sp_team2_score AS 'SP Score'," +
+                    "ma.ms_team1 || '-' || ma.ms_team2 AS 'MS Score'," +
+                    "ma.match_date AS 'Match Date'" +
                     " FROM match_additions AS ma" +
                     " LEFT JOIN teams AS team1 ON team1.id = ma.team1_id" +
                     " LEFT JOIN teams AS team2 ON team2.id = ma.team2_id";
